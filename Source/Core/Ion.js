c69c8f6a6ba3cb366600a8b18e2c5ede1d9bb186
TomBlack01 <514356933@qq.com>
2021-05-08 11:27:08 +0800

    token01

--- Source/Core/Ion.js
+++ Source/Core/Ion.js
@@ -5,6 +5,8 @@ import Resource from "./Resource.js";
 var defaultTokenCredit;
 var defaultAccessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2N2NjZjU1MS02ZDE5LTRjYzAtYjg0Zi0wODc2YjMxNGE1ZWEiLCJpZCI6MjU5LCJpYXQiOjE2MjAwNDczMDl9.159vCWIGDh0dexF393wvC4zUCbq4oZg6VkPAB5U6tg0";
+defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2N2NjZjU1MS02ZDE5LTRjYzAtYjg0Zi0wODc2YjMxNGE1ZWEiLCJpZCI6MjU5LCJpYXQiOjE2MjAwNDczMDl9.159vCWIGDh0dexF393wvC4zUCbq4oZg6VkPAB5U6tg0";
 
 /**
  * Default settings for accessing the Cesium ion API.
