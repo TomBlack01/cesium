67fb4d7288e93c05869b188addcbdf6371c95414
TomBlackTan <514356933@qq.com>
2021-05-08 17:46:55 +0800

    添加测试demo

--- Apps/Sandcastle/load-cesium-es6.js
+++ Apps/Sandcastle/load-cesium-es6.js
@@ -1,6 +1,10 @@
 // This file loads the unbuilt ES6 version of Cesium
 // into the global scope during local developmnet
-window.CESIUM_BASE_URL = "../../../Source/";
+window.CESIUM_BASE_URL = "../../../Source/"; //gallery原目录下测试文件
+// if (window.location.href.indexOf("/0-trs/") > -1) { //我的自定义测试资源
+//     window.CESIUM_BASE_URL = "../../../../Source/";
+// }
+
 import * as Cesium from "../../Source/Cesium.js";
 window.Cesium = Cesium;
 
